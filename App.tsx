6625db1d9fe9c830a615610e39558e050d5a366c
gsbenevides2 <gsbenevides2@gmail.com>
2020-09-23 22:57:37 +0000

    Initial Commit

---
+++ App.tsx
@@ -0,0 +1,25 @@
+// eslint-disable-next-line no-use-before-define
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+import { StatusBar } from 'expo-status-bar'
+
+const App: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
+
+export default App
